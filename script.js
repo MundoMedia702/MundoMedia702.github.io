39c56e6df375a04aa33d6b75d623569f0a2cf808
MundoMedia702 <bitcrash08@gmail.com>
2024-09-20 12:43:14 -0500

    Nuevos estilos y enlaces nuevos

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-function toggleMenu() {
-    var sidebar = document.getElementById("sidebar");
-    sidebar.classList.toggle("show");
-}
-
+// Funciones de contenido
 function showContent(sectionId) {
     const section = document.getElementById(sectionId);
     const content = section.querySelector('.content').innerHTML;
@@ -12,9 +8,9 @@ function showContent(sectionId) {
 
 function searchInSection(sectionId, searchTerm) {
     const section = document.getElementById(sectionId);
-    const contentDiv = section.querySelector('.content');
+    const contentDiv = section.querySelector('.scrollable');
     const links = contentDiv.querySelectorAll('a');
-    
+
     links.forEach(link => {
         if (link.textContent.toLowerCase().includes(searchTerm.toLowerCase())) {
             link.style.display = 'block';
@@ -26,10 +22,10 @@ function searchInSection(sectionId, searchTerm) {
 
 function generalSearch() {
     const searchTerm = document.getElementById('general-search').value.toLowerCase();
-    const sections = document.querySelectorAll('.section');   
+    const sections = document.querySelectorAll('.section');
 
     sections.forEach(section => {
-        const links = section.querySelectorAll('.content a');
+        const links = section.querySelectorAll('.scrollable a');
         links.forEach(link => {
             if (link.textContent.toLowerCase().includes(searchTerm)) {
                 link.style.display = 'block';
@@ -39,3 +35,28 @@ function generalSearch() {
         });
     });
 }
+
+// Funciones del menú lateral
+const menuToggle = document.getElementById('menu-toggle');
+const sidebar = document.querySelector('.sidebar');
+const links = document.querySelectorAll('.sidebar .file');
+
+menuToggle.addEventListener('click', () => {
+    sidebar.style.display = sidebar.style.display === 'block' ? 'none' : 'block';
+});
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        sidebar.style.display = 'none'; // Cerrar el menú
+    });
+});
+
+// Funcionalidad para copiar la dirección de la wallet
+document.getElementById('copy-button').addEventListener('click', function() {
+    const walletAddress = '1PpYBzQABU4woy2ShuPAnm6smC9REQxfHG';
+    navigator.clipboard.writeText(walletAddress).then(() => {
+        alert('Dirección copiada al portapapeles!');
+    }, (err) => {
+        console.error('Error al copiar: ', err);
+    });
+});
